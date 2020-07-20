6b989e201cbafce01ba927a0fef319e624b2d197
zoggn <kurkov.nikita@gmail.com>
2020-07-20 19:37:21 +0300

    N1: reverse imgsensor_pd_info from stock kernel
    Fixed Error:
       Set PD algo failed! close PDAF

--- drivers/misc/mediatek/imgsensor/src/mt6757/imx258_mipi_raw/imx258mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt6757/imx258_mipi_raw/imx258mipiraw_Sensor.c
@@ -306,11 +306,11 @@ static SET_PD_BLOCK_INFO_T imgsensor_pd_info = {
 	.i4OffsetY = 24,
 	.i4PitchX = 32,
 	.i4PitchY = 32,
-	.i4PairNum = 8,
-	.i4SubBlkW = 8,
+	.i4PairNum = 4,
+	.i4SubBlkW = 16,
 	.i4SubBlkH = 16,
-	.i4PosL = {{26, 29}, {34, 29}, {42, 29}, {50, 29}, {30, 45}, {38, 45}, {46, 45}, {54, 45} },
-	.i4PosR = {{26, 33}, {34, 33}, {42, 33}, {50, 33}, {30, 49}, {38, 49}, {46, 49}, {54, 49} },
+	.i4PosL = {{26, 29}, {42, 29}, {33, 48}, {49, 48} },
+	.i4PosR = {{25, 32}, {41, 32}, {34, 45}, {50, 45} },
 	.iMirrorFlip = 0,	/* 0:IMAGE_NORMAL,1:IMAGE_H_MIRROR,2:IMAGE_V_MIRROR,3:IMAGE_HV_MIRROR */
 };
 
